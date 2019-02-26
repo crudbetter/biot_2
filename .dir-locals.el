c5ece5f7a03f0b319f3dd68a787e43ac05e89074
Mike Godfrey <mike@crudbetter.com>
2019-02-26 07:29:41 +0000

    Directory flattening

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,6 +1,6 @@
 ((elm-mode
   (elm-interactive-command . ("elm" "repl"))
   (elm-compile-command . ("elm" "make"))
-  (elm-compile-arguments . ("--output=../priv/static/main.js"))
+  (elm-compile-arguments . ("--output=priv/static/main.js"))
   (elm-package-command . ("elm" "package"))
   (elm-package-json . "elm.json")))
